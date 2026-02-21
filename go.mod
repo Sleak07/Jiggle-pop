5d55d71498cbbaf6d39b21c6db30696681a0e87f
Bimal Mallick <ghostridergothan8460@gmail.com>
2026-02-21 19:36:58 +0530

    new files

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Sleak07/Jiggle-pop
+module github.com/Sleak07/Jiggle-pop.git
 
-go 1.24.9
+go 1.25.7
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
+// packages in go
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
 
 func main() {
-	fmt.Println("Hello from Jiggle tools 🍮")
+	fmt.Println("Hello world")
+	fmt.Println("My favorite number is ", rand.Intn(12))
+
+	// exported names have a capital letter
+	fmt.Println(math.Pi)
+	fmt.Println(add(23, 56))
+}
+
+// functions in go
+func add(x int, y int) int {
+	return x + y
 }
