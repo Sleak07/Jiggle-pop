04b5138cf00bef8d02c76c798d35ee0803167241
Bimal Mallick <ghostridergothan8460@gmail.com>
2026-04-18 19:25:27 +0530

    Completed it

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 // TODO: Take user input and count the number of words and find the longest word
 package main
 
-import(
-	"fmt"
+import (
 	"bufio"
+	"fmt"
 	"os"
 	"strings"
 )
 
 func main() {
 	fmt.Print("Enter a word of your Choice: ")
+	user := bufio.NewScanner(os.Stdin)
+	user.Scan()
 
+	userInput := strings.TrimSpace(user.Text())
 
+	// splitting the sentence to words
+	word := strings.Fields(userInput)
+
+	// Getting the count
+	wordCount := len(word)
+
+	// Getting longest word
+
+	longestWord := ""
+	for _, wod := range word {
+		if len(wod) > len(longestWord) {
+			longestWord = wod
+		}
+	}
+	fmt.Printf("Word count   : %d\n", wordCount)
+	fmt.Printf("Longest word : %s\n", longestWord)
 }
