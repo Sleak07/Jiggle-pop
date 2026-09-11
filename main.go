0461207a2f059d93f93c27bc130f1e1ac874cbe4
Bimal Mallick <ghostridergothan8460@gmail.com>
2026-09-11 19:10:28 +0530

    use of case switch in go

--- main.go
+++ main.go
@@ -3,11 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	greeting := greet()
+	greeting := greet("fr")
 	fmt.Println(greeting)
 }
 
+// language represents language's code
+type language string
+
 // greet returns greeting to world
-func greet() string {
-	return "Hello World"
+func greet(l language) string {
+	switch l {
+	case "en":
+		return "Hello World"
+	case "fr":
+		return "Bonjour le monde"
+	default:
+		return ""
+	}
 }
--- main_internal_test.go
+++ main_internal_test.go
@@ -2,10 +2,33 @@ package main
 
 import "testing"
 
-func TestGreet(t *testing.T) {
+func TestGreet_English(t *testing.T) {
+	lang := language("en")
 	want := "Hello World"
-	got := greet()
 
+	got := greet(lang)
+	if got != want {
+		// mark test failed
+		t.Errorf("expected: %q, got:%q", want, got)
+	}
+}
+
+func TestGreet_French(t *testing.T) {
+	lang := language("fr")
+	want := "Bonjour le monde"
+
+	got := greet(lang)
+	if got != want {
+		// mark test failed
+		t.Errorf("expected: %q, got:%q", want, got)
+	}
+}
+
+func TestGreet_Akkadian(t *testing.T) {
+	lang := language("ak")
+	want := ""
+
+	got := greet(lang)
 	if got != want {
 		// mark test failed
 		t.Errorf("expected: %q, got:%q", want, got)
